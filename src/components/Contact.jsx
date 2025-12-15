1ec7b82b4f3d44ab3faad45f6689903c5f3e4462
dushyant kumar <dushyantvelar@gmail.com>
2025-12-15 23:53:14 +0530

    add the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,27 +7,29 @@ const Contact = () => {
     <div className="px-20 flex flex-col gap-5">
       <Title title={"Get in Touch"} />
       <div className="flex justify-between px-10">
-        <div className="flex flex-col gap-5">
-          <div className="flex items-center px-5 py-2 text-white bg-[linear-gradient(135deg,#844cff,#11bfff)] ">
-            <span>
-              <i class="fa-solid fa-phone"></i>
+        <div className="flex flex-col gap-5 place-self-end">
+          <div className="flex items-center gap-2 pl-5 pr-30 py-2 text-white bg-[linear-gradient(135deg,#844cff,#11bfff)] -skew-x-10 ">
+            <span className="">
+              <i class="fa-solid fa-phone text-3xl"></i>
             </span>
             <div className="flex flex-col">
               <p>Phone</p>
               <p>+91 9742284903</p>
             </div>
           </div>
-          <div className="flex px-5 py-2 text-white bg-[linear-gradient(135deg,#844cff,#11bfff)]">
-            <span>
-              <i class="fa-solid fa-globe"></i>
+          <div className="flex items-center gap-2 pl-5 pr-30 py-2 text-white bg-[linear-gradient(135deg,#844cff,#11bfff)] -skew-x-10">
+            <span className="">
+              <i className="fa-solid fa-globe text-3xl"></i>
             </span>
             <div className="flex flex-col">
               <p>Website</p>
               <p>www.futise.com</p>
             </div>
           </div>
-          <div className="flex px-5 py-2 text-white bg-[linear-gradient(135deg,#844cff,#11bfff)]">
-            <span> </span>
+          <div className="flex items-center gap-2 pl-5 pr-30 py-2 text-white bg-[linear-gradient(135deg,#844cff,#11bfff)] -skew-x-10">
+            <span>
+              <i class="fa-solid fa-location-dot text-3xl"></i>
+            </span>
             <div className="flex flex-col">
               <p>Registered Address</p>
               <p>Hubli, Karnataka, India</p>
@@ -35,7 +37,7 @@ const Contact = () => {
           </div>
         </div>
         <div className="">
-          <img src={img} alt="" className="w-70 h-60" />
+          <img src={img} alt="" className="w-90 h-70" />
         </div>
       </div>
     </div>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,9 +3,9 @@ import logo from "../assets/images/futise2.jpeg"
 
 const Title = ({title}) => {
   return (
-    <div className="flex justify-between items-center">
-      <h1 className="text-2xl font-bold ">{title}</h1>
-      <img src={logo} alt="" className="size-10"/>
+    <div className="flex justify-between items-center mb-2">
+      <h1 className="text-3xl font-bold ">{title}</h1>
+      <img src={logo} alt="" className="size-15"/>
     </div>
   );
 };
